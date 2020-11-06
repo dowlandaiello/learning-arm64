ca0127b5dc5542918feaa6a994db43193a41b013
Dowland Aiello <dowlandaiello@gmail.com>
2020-11-06 08:33:29 -0500

    create hello_world.asm
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

---
+++ hello_world.asm
@@ -0,0 +1,23 @@
+	.global _start
+	.text
+_start:
+	// STDOUT uses fd 1
+	mov 	x0, #1
+	ldr	x1, msg
+	ldr	x2, m_len
+
+	// write syscall is #64
+	mov	x8, #64
+	// Run write()
+	svc	#0
+
+	// Exit
+	mov	x8, #93
+	// No error:
+	mov	x0, #0
+	svc	#0
+
+
+	.data
+msg: .ascii "Hello, World!\n"
+m_len = . - msg
