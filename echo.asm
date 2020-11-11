320395853ff94856c5586c9980dc4a74569907ec
Dowland Aiello <dowland@heywear.com>
2020-11-10 23:23:28 -0500

    fixed
    Signed-off-by: Dowland Aiello <dowland@heywear.com>

--- echo.asm
+++ echo.asm
@@ -4,9 +4,11 @@ prompt:
 	.ascii	"> "
 len = . - prompt
 
+.bss
+
 in:
 	/* Allow 80 chars to be inputted per line */
-	.skip	80, '\0'
+	.skip	80
 
 .text
 
@@ -22,27 +24,27 @@ _start:
 	bl	read_input
 
 	mov	x0, #0
-	mov	x7, #1
+	mov	x8, #93
 	svc	#0
 
 read_input:
 	/* Get up to 80 chars */
 	mov	x8, #63
 	mov	x0, #0
-	mov	x1, in
+	ldr	x1, =in
 	mov	x2, #79
 	svc	#0
 
 	bl echo_input
 
-	ret
+	bl _start
 
 echo_input:
 	/* Print the input */
 	mov	x8, #64
 	mov	x0, #1
 	ldr	x1, =in
-	ldr	x2, 80
+	mov	x2, #80
 	svc	#0
 
 	ret
