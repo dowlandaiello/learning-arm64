22e7bc7cc7ad477778383df908c1e5fc93963b1b
Dowland Aiello <dowland@heywear.com>
2020-11-13 16:32:48 -0500

    use 32bit registes
    Signed-off-by: Dowland Aiello <dowland@heywear.com>

--- pi_blink.asm
+++ pi_blink.asm
@@ -3,17 +3,17 @@
 .globl _start
 _start:
 	/* Set pin 47 as an output */
-	mov	r0, #1
-	lsl	r0, #21
+	mov	w0, #1
+	lsl	w0, #21
 
 	/* 4th GPIO register, FSEL47 */
-	ldr	r1, =0x20200010
-	str	r1, r0
+	ldr	w1, =0x20200010
+	str	w1, w0
 
 	/* Set pin 47 to on */
-	mov	r0, #1
-	lsl	r0, #16
+	mov	w0, #1
+	lsl	w0, #16
 	
 	/* GPIO output set register 1 */
-	ldr	r1, =0x20200020
-	str	r1, r0
+	ldr	w1, =0x20200020
+	str	w1, w0
