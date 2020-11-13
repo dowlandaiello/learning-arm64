bc9c5b28856efd4cd4228427a82d21f321138bf0
Dowland Aiello <dowlandaiello@gmail.com>
2020-11-13 17:11:39 -0500

    yay it works
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- pi_blink.asm
+++ pi_blink.asm
@@ -3,17 +3,17 @@
 .globl	_start
 _start:
 	/* Set pin 47 as an output */
-	mov	w0, #1
-	lsl	w0, w0, #21
+	mov	r0, #1
+	lsl	r0, r0, #21
 
 	/* 4th GPIO register, FSEL47 */
-	ldr	x1, =0x20200010
-	str	w0, [x1]
+	ldr	r1, =0x20200010
+	str	r0, [r1]
 
 	/* Set pin 47 to on */
-	mov	w0, #1
-	lsl	w0, w0, #16
+	mov	r0, #1
+	lsl	r0, r0, #16
 
 	/* GPIO output set register 1 */
-	ldr	x1, =0x20200020
-	str	w0, [x1]
+	ldr	r1, =0x20200020
+	str	r0, [r1]
