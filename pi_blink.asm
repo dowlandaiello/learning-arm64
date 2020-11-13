88d1783004ea19b84e12e7928ceec0b11b69001a
Dowland Aiello <dowland@heywear.com>
2020-11-13 16:42:45 -0500

    here goes nothing...
    Signed-off-by: Dowland Aiello <dowland@heywear.com>

--- pi_blink.asm
+++ pi_blink.asm
@@ -1,19 +1,19 @@
 .text
 
-.globl _start
+.globl	_start
 _start:
 	/* Set pin 47 as an output */
 	mov	w0, #1
-	lsl	w0, #21
+	lsl	w0, w0, #21
 
 	/* 4th GPIO register, FSEL47 */
-	ldr	w1, =0x20200010
-	str	w1, w0
+	ldr	x1, =0x20200010
+	str	w0, [x1]
 
 	/* Set pin 47 to on */
 	mov	w0, #1
-	lsl	w0, #16
-	
+	lsl	w0, w0, #16
+
 	/* GPIO output set register 1 */
-	ldr	w1, =0x20200020
-	str	w1, w0
+	ldr	x1, =0x20200020
+	str	w0, [x1]
